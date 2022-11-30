ae4fba934e8e7875dadcfcbda2cd1e9d03f53465
TAKAMI1228 <kanakae187@gmail.com>
2022-12-01 08:02:00 +0900

    feat:dialogの追加

--- src/Day.svelte
+++ src/Day.svelte
@@ -1,5 +1,24 @@
 <script lang="ts">
+import Dialog from './Dialog.svelte';
   export let date: Date;
+
+  let dialog;
+  let today = new Date();
+
+  var day = today.getDate();
+
+  function openDialog() {
+    dialog.showModal()
+    dialog.addEventListener('click', function (event) {
+      if (event.target === dialog) {
+        dialog.close()
+      }
+    })
+  }
+
+  function closeDialog() {
+    dialog.close()
+  }
 </script>
 
 <style>
@@ -7,12 +26,28 @@
     flex: 1;
     border: 1px solid #ccc;
   }
+  button {
+    margin: 5px;
+  }
 </style>
 
 <div>
   {#if date.getDate() === 1}
-    <a href="#">{date.getMonth() + 1}/{date.getDate()}</a>
+    {#if date.getDate() === day}
+    <button on:click={openDialog}>{date.getMonth() + 1}/{date.getDate()}</button>
+    {:else}
+    {date.getMonth() + 1}/{date.getDate()}
+    {/if}
   {:else}
-    <a href="#">{date.getDate()}</a>
+    {#if date.getDate() === day}
+    <button>{date.getDate()}</button>
+    {:else}
+    {date.getDate()}
+    {/if}
   {/if}
 </div>
+
+<Dialog
+	bind:dialog
+	on:closeDialog={closeDialog}
+/>
---
+++ src/Dialog.svelte
@@ -0,0 +1,22 @@
+<script>
+    import { createEventDispatcher } from 'svelte'; // 子から親への値の受け渡し
+
+	export let dialog;
+
+	const dispatch = createEventDispatcher()
+    function clickClose() {
+        dispatch('closeDialog');
+    }
+</script>
+
+<dialog bind:this={dialog}>
+  <div class='inner'>
+	<p>本日は</p>
+	<h2>
+		チョコレート
+    </h2>
+	<button on:click={clickClose}>
+		close
+	</button>
+  </div>
+</dialog>
